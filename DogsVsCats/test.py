22adf230042f60776cad95a49c5980afc5702205
hgsong <hgsong@wantedlab.com>
2018-05-24 12:12:27 +0900

    classify dogs and cats

---
+++ DogsVsCats/test.py
@@ -0,0 +1,205 @@
+import matplotlib.pyplot as plt
+import os
+import numpy as np
+from scipy.misc import imresize
+from sklearn.model_selection import train_test_split
+import pandas as pd
+from datetime import timedelta
+
+train_path = '/Users/ross/testKaggle/DogsVsCats/train/'
+test_path = '/Users/ross/testKaggle/DogsVsCats/test1/'
+
+def get_label(path):
+    labels = []
+    for i in os.listdir(path):
+        if 'cat' in i:
+            labels.append(0)
+        else:
+            labels.append(1)
+    return labels
+
+def get_array(path):
+    cnt = 0
+    img_array = []
+    for i in os.listdir(path):
+        img_array.append(imresize(plt.imread(path+i), (120, 120, 3)))
+        cnt+=1
+        print(cnt)
+    return img_array
+
+label = get_label(train_path)
+array = get_array(train_path)
+
+
+train_x, test_x, train_y, test_y = train_test_split(array, label, test_size=0.2)
+test_set = pd.DataFrame({'x': test_x, 'y': test_y})
+
+import tensorflow as tf
+import pandas as pd
+from sklearn import metrics
+import pickle
+import numpy as np
+import time
+import random
+
+# parameters
+LEARNING_RATE = 0.001
+DECAY_RATE = 0.9
+
+OUTPUT_SIZE = 2
+KERNEL=[3, 2]
+NUM_FEATURE=[32, 64, 128, 256, 512]
+
+#initializers_초기화 함수)
+initializer = {
+    'truncated': tf.truncated_normal_initializer(),
+    'xavier': tf.contrib.layers.xavier_initializer(),
+    'he': tf.contrib.keras.initializers.he_normal()}
+
+activations = {
+    'sigmoid' : tf.nn.sigmoid,
+    'relu' : tf.nn.relu,
+    'lrelu' : tf.nn.leaky_relu,
+'elu':tf.nn.elu}
+
+def weight_variable(shape, init, name):
+    return tf.get_variable(shape = shape, initializer=initializer[init], name=name)
+def bias_variable(shape, name):
+    return tf.get_variable(shape=shape, initializer= tf.zeros_initializer(), name=name)
+def max_pool(inputs, kernel, stride):
+    return tf.contrib.layers.max_pool2d(
+        inputs,
+        kernel_size=[kernel, kernel],
+        stride=stride,
+        padding='SAME',
+        outputs_collections=None,
+        scope=None)
+def batch_normalization(x, training, scope):
+    with tf.contrib.framework.arg_scope([tf.contrib.layers.batch_norm],
+                                        scope=scope,
+                                        updates_collections=None,
+                                        decay=0.9,
+                                        center=True,
+                                        scale=True,
+                                        zero_debias_moving_mean=True):
+        return tf.cond(training,
+                       lambda: tf.contrib.layers.batch_norm(inputs=x, is_training=training, reuse=None),
+                       lambda: tf.contrib.layers.batch_norm(inputs=x, is_training=training, reuse=True))
+def conv(inpunt, num_featrue, kernel, act, init, con = None) :
+    if con ==None:
+        return tf.contrib.layers.conv2d(inpunt,
+                             num_outputs = num_featrue,
+                             kernel_size = [kernel, kernel],
+                             padding = 'SAME',
+                             activation_fn = act,
+                             weights_initializer = init,
+                             biases_initializer = tf.zeros_initializer)
+    elif con =='de':
+        return tf.contrib.layers.conv2d_transpose(inpunt,
+                                                 num_outputs = num_featrue,
+                                                 kernel_size = [kernel, kernel],
+                                                 padding='SAME',
+                                                 activation_fn = act,
+                                                 weights_initializer=init,
+                                                 biases_initializer=tf.zeros_initializer
+                                                 )
+def batch_conv(inpunt, num_featrue, kernel, act, init, training, scope) :
+    conv = tf.contrib.layers.conv2d(inpunt,
+                             num_outputs = num_featrue,
+                             kernel_size = [kernel, kernel],
+                             padding = 'SAME',
+                             activation_fn = None,
+                             weights_initializer = init,
+                             biases_initializer = tf.zeros_initializer)
+    conv = batch_normalization(conv, training, scope)
+    conv = act(conv)
+    return conv
+
+tf.reset_default_graph()
+
+input_x = tf.placeholder(tf.float32, [None, None, None, None])
+input_y = tf.placeholder(tf.float32, [None, None])
+training = tf.placeholder(tf.bool, name='training')
+keep_prob = tf.placeholder(tf.float32)
+
+# batch normalization + convolutional layers
+la_active = activations['elu']
+c_initializer = initializer['he']
+x_image = tf.reshape(input_x, [-1, 120, 120, 3])
+conv_layer1 = batch_conv(x_image, NUM_FEATURE[0], KERNEL[0], la_active, c_initializer, training, 'conv1')
+conv_layer2 = batch_conv(conv_layer1, NUM_FEATURE[0], KERNEL[0], la_active, c_initializer, training, 'conv2')
+conv_layer3 = batch_conv(conv_layer2, NUM_FEATURE[0], KERNEL[0], la_active, c_initializer, training, 'conv3')
+pool1 = max_pool(conv_layer3, KERNEL[0], 2)
+
+conv_layer4 = batch_conv(pool1, NUM_FEATURE[1], KERNEL[1], la_active, c_initializer, training, 'conv4')
+conv_layer5 = batch_conv(conv_layer4, NUM_FEATURE[1], KERNEL[1], la_active, c_initializer, training, 'conv5')
+conv_layer6 = batch_conv(conv_layer5, NUM_FEATURE[1], KERNEL[1], la_active, c_initializer, training, 'conv6')
+pool2 = max_pool(conv_layer6, KERNEL[0], 2)
+
+conv_layer7 = batch_conv(pool2, NUM_FEATURE[2], KERNEL[1], la_active, c_initializer, training, 'conv7')
+conv_layer8 = batch_conv(conv_layer7, NUM_FEATURE[2], KERNEL[1], la_active, c_initializer, training, 'conv8')
+pool3 = max_pool(conv_layer8, KERNEL[0], 2)
+
+conv_layer9 = batch_conv(pool3, NUM_FEATURE[3], KERNEL[1], la_active, c_initializer, training, 'conv9')
+conv_layer10 = batch_conv(conv_layer9, NUM_FEATURE[3], KERNEL[1], la_active, c_initializer, training, 'conv10')
+pool5 = max_pool(conv_layer10, KERNEL[0], 2)
+
+layer_shape = pool5.shape
+re_img = tf.reshape(pool5, [-1, layer_shape[1]*layer_shape[2]*layer_shape[3]])
+
+fully_layer = tf.contrib.layers.fully_connected(re_img, 2048, activation_fn= tf.nn.elu,
+                                                weights_initializer = c_initializer,
+                                                biases_initializer = tf.zeros_initializer)
+batch_fully = batch_normalization(fully_layer, training, 'fully')
+
+fully_layer1 = tf.contrib.layers.fully_connected(batch_fully, 2048, activation_fn= tf.nn.elu,
+                                                weights_initializer = c_initializer,
+                                                biases_initializer = tf.zeros_initializer)
+batch_fully1 = batch_normalization(fully_layer1, training, 'fully1')
+
+score = tf.contrib.layers.fully_connected(batch_fully1, OUTPUT_SIZE, activation_fn = None)
+
+# Cost to be optimized
+cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits_v2(labels=input_y, logits=score))
+
+# Optimization method
+optimizer = tf.train.AdamOptimizer(LEARNING_RATE).minimize(cost)
+
+# Performance measures
+prediction = tf.argmax(score, 1)
+softmax = tf.nn.softmax(logits=score)
+correct_prediction = tf.equal(prediction, tf.argmax(input_y, 1))
+accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+
+BATCH_SIZE = 128
+EPOCH = 500
+cnt=0
+saver = tf.train.Saver()
+with tf.Session() as sess:
+    sess.run(tf.global_variables_initializer())
+    start_time = time.time()
+    for i in range(EPOCH):
+        training_batch = zip(range(0, len(train_x), BATCH_SIZE), range(BATCH_SIZE, len(train_x), BATCH_SIZE))
+        for start, end in training_batch:
+            cnt+=1
+            _, train_cost, train_accuracy = sess.run([optimizer, cost, accuracy],
+                                                           feed_dict={input_x: list(train_x)[start:end],
+                                                                      input_y: list(pd.get_dummies(train_y).values[start:end]),
+                                                                      training: True})
+
+            dev_idx = random.sample(range(len(test_set)), BATCH_SIZE)
+            valid_cost, valid_accuracy = sess.run([cost, accuracy],
+                                      feed_dict={input_x: list(test_set.x.iloc[dev_idx].values),
+                                                 input_y: list(pd.get_dummies(test_set.y[dev_idx]).values),
+                                                 training: False})
+            end_time = time.time()
+            time_dif = end_time - start_time
+
+            print('======================================')
+            print("Epoch: {:g}, Step: {:g}".format(i, cnt))
+            print("Train loss: {:g}, Train acc: {:g}".format(train_cost, train_accuracy))
+            print("Valid loss: {:g}, Valid acc: {:g}".format(valid_cost, valid_accuracy))
+            print("Time usage: " + str(timedelta(seconds=int(round(time_dif)))))
+
+        saver.save(sess, "/Users/ross/testKaggle/DogsVsCats("+str(i)+")")
+
---
+++ DogsVsCats/test2.py
@@ -0,0 +1,94 @@
+import matplotlib.pyplot as plt
+import os
+import numpy as np
+from scipy.misc import imresize
+from sklearn.model_selection import train_test_split
+import pandas as pd
+from datetime import timedelta
+import pickle
+
+train_path = '/Users/ross/testKaggle/DogsVsCats/train/'
+test_path = '/Users/ross/testKaggle/DogsVsCats/test1/'
+
+def get_label(path):
+    labels = []
+    for i in os.listdir(path):
+        if 'cat' in i:
+            labels.append(0)
+        else:
+            labels.append(1)
+    return labels
+
+def get_array(path):
+    cnt = 0
+    img_array = []
+    for i in os.listdir(path):
+        img_array.append(imresize(plt.imread(path+i), (224, 224, 3)))
+        cnt+=1
+        print(cnt)
+    return img_array
+
+label = get_label(train_path)
+array = get_array(train_path)
+
+train_x, test_x, train_y, test_y = train_test_split(array, label, test_size=0.2)
+test_set = pd.DataFrame({'x': test_x, 'y': test_y})
+
+train_x = np.array(train_x)
+test_x = np.array(test_x)
+
+train_x = train_x.reshape(train_x.shape[0], 224, 224, 3)
+test_x = test_x.reshape(test_x.shape[0], 224, 224, 3)
+
+print(train_x.shape)
+print(test_x.shape)
+
+train_y = pd.get_dummies(train_y).values
+
+import pandas as pd
+from sklearn import metrics
+import pickle
+import numpy as np
+import time
+import random
+from keras.models import Sequential
+from keras.layers import Dense, Activation, Dropout, Flatten
+from keras.optimizers import RMSprop
+from keras.layers import Conv2D
+from keras.layers import MaxPooling2D
+
+input_shape = (224, 224, 3)
+BATCH_SIZE = 128
+EPOCH = 500
+
+model = Sequential([
+    Conv2D(64, (3, 3), input_shape=input_shape, padding='same',
+           activation='relu'),
+    Conv2D(64, (3, 3), activation='relu', padding='same'),
+    MaxPooling2D(pool_size=(2, 2), strides=(2, 2)),
+    Conv2D(128, (3, 3), activation='relu', padding='same'),
+    Conv2D(128, (3, 3), activation='relu', padding='same',),
+    MaxPooling2D(pool_size=(2, 2), strides=(2, 2)),
+    Conv2D(256, (3, 3), activation='relu', padding='same',),
+    Conv2D(256, (3, 3), activation='relu', padding='same',),
+    Conv2D(256, (3, 3), activation='relu', padding='same',),
+    MaxPooling2D(pool_size=(2, 2), strides=(2, 2)),
+    Conv2D(512, (3, 3), activation='relu', padding='same',),
+    Conv2D(512, (3, 3), activation='relu', padding='same',),
+    Conv2D(512, (3, 3), activation='relu', padding='same',),
+    MaxPooling2D(pool_size=(2, 2), strides=(2, 2)),
+    Conv2D(512, (3, 3), activation='relu', padding='same',),
+    Conv2D(512, (3, 3), activation='relu', padding='same',),
+    Conv2D(512, (3, 3), activation='relu', padding='same',),
+    MaxPooling2D(pool_size=(2, 2), strides=(2, 2)),
+    Flatten(),
+    Dense(4096, activation='relu'),
+    Dense(4096, activation='relu'),
+    Dense(2, activation='softmax')
+])
+
+model.compile(optimizer=RMSprop(lr=0.001, rho=0.9), loss='categorical_crossentropy', metrics=['accuracy'])
+model.fit(train_x, train_y, epochs=EPOCH, batch_size=BATCH_SIZE, verbose=1)
+
+evaluation = model.evaluate(test_x, test_y, verbose=1)
+print('Summary: Loss over the test dataset: %.2f, Accuracy: %.2f' % (evaluation[0], evaluation[1]))

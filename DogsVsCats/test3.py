f606671fa2f78a80c8d73774cfa6fe44751d2370
hgsong <hgsong@wantedlab.com>
2018-05-24 19:51:51 +0900

    transfer learning add

---
+++ DogsVsCats/test3.py
@@ -0,0 +1,84 @@
+import matplotlib.pyplot as plt
+from keras.applications.vgg16 import VGG16
+import os
+import numpy as np
+from scipy.misc import imresize
+from sklearn.model_selection import train_test_split
+import pandas as pd
+from datetime import timedelta
+import pickle
+
+train_path = '/Users/ross/testKaggle/DogsVsCats/train/'
+test_path = '/Users/ross/testKaggle/DogsVsCats/test1/'
+
+def get_label(path):
+    labels = []
+    for i in os.listdir(path):
+        if 'cat' in i:
+            labels.append(0)
+        else:
+            labels.append(1)
+    return labels
+
+def get_array(path):
+    cnt = 0
+    img_array = []
+    for i in os.listdir(path):
+        img_array.append(imresize(plt.imread(path+i), (224, 224, 3)))
+        cnt+=1
+        print(cnt)
+    return img_array
+
+label = get_label(train_path)
+array = get_array(train_path)
+
+train_x, test_x, train_y, test_y = train_test_split(array, label, test_size=0.2)
+test_set = pd.DataFrame({'x': test_x, 'y': test_y})
+
+train_x = np.array(train_x)
+test_x = np.array(test_x)
+
+train_x = train_x.reshape(train_x.shape[0], 224, 224, 3)
+test_x = test_x.reshape(test_x.shape[0], 224, 224, 3)
+
+print(train_x.shape)
+print(test_x.shape)
+#plt.imshow(train_x[0])
+#plt.show()
+
+
+train_y = pd.get_dummies(train_y).values
+#print(train_y[0])
+
+import pandas as pd
+from sklearn import metrics
+import pickle
+import numpy as np
+import time
+import random
+from keras.models import Sequential, Model
+from keras.layers import Dense, Activation, Dropout, Flatten
+from keras import optimizers
+from keras.optimizers import RMSprop
+from keras.layers import Conv2D
+from keras.layers import MaxPooling2D
+
+input_shape = (224, 224, 3)
+BATCH_SIZE = 128
+EPOCH = 500
+
+base_model = VGG16(weights='imagenet', include_top=False, input_shape=input_shape)
+add_model = Sequential()
+add_model.add(Flatten(input_shape=base_model.output_shape[1:]))
+add_model.add(Dense(256, activation='relu'))
+add_model.add(Dense(2, activation='sigmoid'))
+
+model = Model(inputs=base_model.input, outputs=add_model(base_model.output))
+model.compile(loss='binary_crossentropy', optimizer=optimizers.SGD(lr=1e-4, momentum=0.9),
+              metrics=['accuracy'])
+
+model.summary()
+model.fit(train_x, train_y, epochs=EPOCH, batch_size=BATCH_SIZE, verbose=1)
+
+evaluation = model.evaluate(test_x, test_y, verbose=1)
+print('Summary: Loss over the test dataset: %.2f, Accuracy: %.2f' % (evaluation[0], evaluation[1]))
